c354673f8f590956e52ed434a0371c5451caec91
panevka <97028404+panevka@users.noreply.github.com>
2026-08-18 17:51:21 +0200

    feat: make get_fc_numeric_string argument fully optional

--- source/lib/fc-utilities.ts
+++ source/lib/fc-utilities.ts
@@ -4,7 +4,7 @@ export function get_fc_string_with_at_least_one_non_digit() {
   return fc.stringMatching(/.*\D.*/);
 }
 
-export function get_fc_numeric_string(options: { min_length?: number; max_length?: number; }) {
+export function get_fc_numeric_string(options?: { min_length?: number; max_length?: number; }) {
   return fc
     .array(fc.integer({ min: 0, max: 9 }), {
       minLength: options?.min_length,
